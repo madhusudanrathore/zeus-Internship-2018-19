63522d9e3cd8f5c2437bc720dd4d70be09d6c7cc
Madhusudan Rathore <madhusudanrathore@protonmail.com.com>
2018-12-17 16:15:50 +0530

    17 dec 2018 16:19 commit 1). added background image in announcements section of day6-7 2). made changes in day 11-12 to make it follow standard practices 3). day 13-15 task 1 completed

--- day11-12/script.js
+++ day11-12/script.js
@@ -1,49 +1,51 @@
 function resetForm(){
-	var userFirstName=document.getElementById("userFirstName");
+	var userFirstName=document.getElementById("user-first-name");
 	userFirstName.focus();
 }
 function maritialStatus(received){
-	var nameOfSpouse=document.getElementById("nameOfSpouse");
-	if( received.id === "marriedRadio" ){
+	var nameOfSpouse=document.getElementById("name-of-spouse");
+	if( received.id === "married-radio" ){
 		nameOfSpouse.readOnly=false;
-	}else if( received.id === "nonMarriedRadio" ){
+	}else if( received.id === "non-married-radio" ){
 		nameOfSpouse.readOnly=true;
 		nameOfSpouse.value="";
 	}
 }
 function onSubmit(){
-	var userFirstName=document.getElementById("userFirstName");
-	var userLastName=document.getElementById("userLastName");
-	var nameOfSpouse=document.getElementById("nameOfSpouse");
-	var genderMale=document.getElementById("maleRadio").checked;
-	var genderFemale=document.getElementById("femaleRadio").checked;
+	var userFirstName=document.getElementById("user-first-name");
+	var userLastName=document.getElementById("user-last-name");
+	var nameOfSpouse=document.getElementById("name-of-spouse");
+	var genderMale=document.getElementById("male-radio").checked;
+	var genderFemale=document.getElementById("female-radio").checked;
 	var maritialStatusMarried=document.getElementById("marriedRadio").checked;
-	var skills=document.getElementById("skillset").value;
-	
+	var skills=document.getElementById("skillset").value;	
 	var checkboxName="hobbies";
-	var hobbies=document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
+	var hobbies=document.querySelectorAll('input[name="' + checkboxName + '"]:checked');
 
 	if( userFirstName.value === "" ){
 		alert("Please enter a First Name");
 		userFirstName.focus();
-	}else if( userLastName.value === "" ){
-		alert("Please enter a Last Name");
-		userLastName.focus();
-	}else if( genderMale===false && genderFemale===false ){
-		alert("Please select a gender");
 	}else if( userFirstName.value.indexOf(' ')>=0 ){
 		alert("First name has whitespace");
 		userFirstName.focus();
+	}else if( userLastName.value === "" ){
+		alert("Please enter a Last Name");
+		userLastName.focus();
 	}else if( userLastName.value.indexOf(' ')>=0 ){
 		alert("Last name has whitespace");
 		userLastName.focus();
+	}else if( genderMale===false && genderFemale===false ){
+		alert("Please select a gender");
 	}else if( hobbies.length === 0 ){
 		alert("Please select at least one hobby");
+	}else if( skills === ""  ){
+		alert("Please select a skill");
+	}else if( maritialStatusMarried === true && nameOfSpouse.value==="" ){
+		alert("Please enter a Spouse name");
+		nameOfSpouse.focus();
 	}else if( maritialStatusMarried === true && nameOfSpouse.value.indexOf(' ')>=0 ){
 		alert("Spouse name has whitespace");
 		nameOfSpouse.focus();
-	}else if( skills ===""  ){
-		alert("Please select a skill");
 	}else{
 		alert("Thank You");
 		return true;
