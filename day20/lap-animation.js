78c83d1cd958f1aa04b254021bdc0b390e3d63bd
Madhusudan Rathore <madhusudanrathore@protonmail.com>
2018-12-18 23:36:08 +0530

    18 dec 2018 23:45 1). day 20 half done

---
+++ day20/lap-animation.js
@@ -0,0 +1,3 @@
+function animateRunning(){
+	var timeControlVariable=setInterval(function(){ run( timeControlVariable ); }, 100);
+}
--- day20/run-animation.js
+++ day20/run-animation.js
@@ -1,25 +1,37 @@
-var timeControlVariable, width=0, iteration=0;
+// implement using class
 
-function animateRunning(){
-	timeControlVariable=setInterval(function(){ run(); }, 100);
+
+var width=0, iteration=0;
+var runnerProgress, tableElement, infoTableString="information-table-player";
+
+function increaseWidth( imgElement, width ){
+
 }
-function run(){
-	var runnerProgress=document.getElementById("runner-progress");
-	var tableElement=document.getElementById("information-table");
 
-	// console.log( width );
-	runnerProgress.style.marginLeft=(width+'px');
+function addTableData( tableElement ){
+	row = tableElement.insertRow(-1);
+	cell1 = row.insertCell(0);
+	cell2 = row.insertCell(1);
+
+	cell1.innerHTML = iteration;
+	cell2.innerHTML = width;
+}
+
+function run( timeControlVariable ){
+	// runnerProgress=document.getElementById("runner-progress-1");
+	document.getElementById("runner-progress-1").style.marginLeft=(width+'px');
+	document.getElementById("runner-progress-2").style.marginLeft=(width+'px');
+	document.getElementById("runner-progress-3").style.marginLeft=(width+'px');
+	document.getElementById("runner-progress-4").style.marginLeft=(width+'px');
 	width += 25;
 	if( width > 1000 ){
 		clearInterval(timeControlVariable);
 	}
 	if( iteration<10 && ( width%100 === 0 ) ){
-		console.log( iteration++ );
-		row = tableElement.insertRow(-1); // -1 indicates append at end
-		cell1 = row.insertCell(0);
-		cell2 = row.insertCell(1);
-	
-		cell1.innerHTML = iteration;
-		cell2.innerHTML = width;
+		// console.log( iteration++ );
+		addTableData( document.getElementById( infoTableString + "1" ) );
+		addTableData( document.getElementById( infoTableString + "2" ) );
+		addTableData( document.getElementById( infoTableString + "3" ) );
+		addTableData( document.getElementById( infoTableString + "4" ) );
 	}
 }
