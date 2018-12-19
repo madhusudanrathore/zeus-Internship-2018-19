89ecd265eea4d3bf93d1182adbdee5d735a56393
Madhusudan Rathore <madhusudanrathore@protonmail.com>
2018-12-19 22:50:39 +0530

    19 dec 2018 22:50 commit, day 18-19 some progress

--- day18-19/script.js
+++ day18-19/script.js
@@ -5,16 +5,20 @@ function startRunning(){
 }
 function run(){
 	var runnerProgress=document.getElementById("runner-progress");
+	var goBtn=document.getElementById("go-btn");
 	var tableElement=document.getElementById("information-table");
+	var rows=tableElement.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
 
 	// console.log( width );
+	console.log(rows);
 	runnerProgress.style.marginLeft=(width+'px');
 	width += 25;
 	if( width > 1000 ){
+		goBtn.innerHTML="RESET";
 		clearInterval(timeControlVariable);
 	}
 	if( iteration<10 && ( width%100 === 0 ) ){
-		console.log( iteration++ );
+		iteration++;
 		row = tableElement.insertRow(-1); // -1 indicates append at end
 		cell1 = row.insertCell(0);
 		cell2 = row.insertCell(1);
@@ -22,4 +26,7 @@ function run(){
 		cell1.innerHTML = iteration;
 		cell2.innerHTML = width;
 	}
+}
+function abc(){
+	alert("asdsa");
 }
