49a28ef3938619545ba062e819fb12687fa25e19
Madhusudan Rathore <madhusudanrathore@protonmail.com.com>
2018-12-19 19:20:02 +0530

    task 20 half done

--- day20/lap-animation.js
+++ day20/lap-animation.js
@@ -1,3 +1,10 @@
 function animateRunning(){
-	var timeControlVariable=setInterval(function(){ run( timeControlVariable ); }, 100);
+	var runner1=new Runner(0);
+	var runner2=new Runner(1);
+	var runner3=new Runner(2);
+	var runner4=new Runner(3);
+
+	var timeControlVariable=setInterval(function(){
+		run( timeControlVariable, runner1, runner2, runner3, runner4 );
+	}, 100);
 }
--- day20/run-animation.js
+++ day20/run-animation.js
@@ -1,37 +1,62 @@
-// implement using class
+var runnerForwardDistances=[25,25,25,25];
+var runnerReverseDistances=[25,25,25,25];
 
-
-var width=0, iteration=0;
-var runnerProgress, tableElement, infoTableString="information-table-player";
-
-function increaseWidth( imgElement, width ){
-
+class Runner{
+	constructor(playNum){
+		this.playerIndex=playNum;
+		this.runnerProgress="runner-progress-" + this.playerIndex;
+		this.infoTableString="information-table-player-" + this.playerIndex;
+		console.log(this.infoTableString);
+		this.width=0, this.rightWidth=0, this.iteration=1;
+		this.reverseWidth=0;
+	}
+	addTableData(reverse){
+		this.tableElement=document.getElementById(this.infoTableString);
+		var row, cell1, cell2;
+		row=this.tableElement.insertRow(-1);
+		cell1=row.insertCell(0);
+		cell2=row.insertCell(1);
+		cell1.innerHTML=this.iteration;
+		if(reverse){
+			cell2.innerHTML=1000+this.rightWidth;
+		}else{
+			cell2.innerHTML=this.width;
+		}
+	}
+	increaseWidth(timeControlVariable){
+		this.width += runnerForwardDistances[this.playerIndex];
+		if(this.width > 1000 ){
+			this.reverseRun(timeControlVariable);
+		}
+		if( (this.iteration<=10) && (this.width%100 === 0)){
+			this.addTableData(false);
+			this.iteration++;
+			document.getElementById(this.runnerProgress).style.marginLeft=(this.width+'px');
+		}
+	}
+	reverseRun(timeControlVariable){
+		if(this.iteration === 11){
+			document.getElementById(this.runnerProgress).src="../images/runningLeft.png";
+			document.getElementById(this.runnerProgress).style.marginRight=('0px');
+		}
+		this.rightWidth+=runnerReverseDistances[this.playerIndex];
+		this.width=this.width-25;
+		if(this.rightWidth > 1000){
+			clearInterval(timeControlVariable);
+		}
+		if( (this.iteration <= 20) && (this.rightWidth%100 === 0)){
+			console.log(this.iteration, this.rightWidth, this.width);
+			this.addTableData(true);
+			this.iteration++;
+			document.getElementById(this.runnerProgress).style.marginRight=(this.rightWidth+'px');
+			document.getElementById(this.runnerProgress).style.marginLeft=(this.width+'px');
+		}
+	}
 }
 
-function addTableData( tableElement ){
-	row = tableElement.insertRow(-1);
-	cell1 = row.insertCell(0);
-	cell2 = row.insertCell(1);
-
-	cell1.innerHTML = iteration;
-	cell2.innerHTML = width;
-}
-
-function run( timeControlVariable ){
-	// runnerProgress=document.getElementById("runner-progress-1");
-	document.getElementById("runner-progress-1").style.marginLeft=(width+'px');
-	document.getElementById("runner-progress-2").style.marginLeft=(width+'px');
-	document.getElementById("runner-progress-3").style.marginLeft=(width+'px');
-	document.getElementById("runner-progress-4").style.marginLeft=(width+'px');
-	width += 25;
-	if( width > 1000 ){
-		clearInterval(timeControlVariable);
-	}
-	if( iteration<10 && ( width%100 === 0 ) ){
-		// console.log( iteration++ );
-		addTableData( document.getElementById( infoTableString + "1" ) );
-		addTableData( document.getElementById( infoTableString + "2" ) );
-		addTableData( document.getElementById( infoTableString + "3" ) );
-		addTableData( document.getElementById( infoTableString + "4" ) );
-	}
+function run(timeControlVariable, runner1, runner2, runner3, runner4){
+	runner1.increaseWidth( timeControlVariable);
+	runner2.increaseWidth( timeControlVariable);
+	runner3.increaseWidth( timeControlVariable);
+	runner4.increaseWidth( timeControlVariable);
 }
